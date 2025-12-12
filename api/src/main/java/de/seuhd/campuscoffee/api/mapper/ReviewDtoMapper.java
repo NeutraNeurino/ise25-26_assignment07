f4f4dcacbacc2c734708da83b6ebab4006e2c3a5
barthelmes <barthelmes@cl.uni-heidelberg.de>
2025-12-12 06:53:49 +0100

    Simplify Dto Mapper

--- api/src/main/java/de/seuhd/campuscoffee/api/mapper/ReviewDtoMapper.java
+++ api/src/main/java/de/seuhd/campuscoffee/api/mapper/ReviewDtoMapper.java
@@ -1,33 +1,20 @@
 package de.seuhd.campuscoffee.api.mapper;
+//extrem gekürzte Version, die andere hats immer zerschossen
 import de.seuhd.campuscoffee.api.dtos.ReviewDto;
 import de.seuhd.campuscoffee.domain.model.objects.Review;
-import de.seuhd.campuscoffee.domain.ports.api.PosService;
-import de.seuhd.campuscoffee.domain.ports.api.UserService;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+
 /**
  * MapStruct mapper for converting between the {@link Review} domain model objects and {@link ReviewDto}s.
- * .
  */
 @Mapper(componentModel = "spring")
 @ConditionalOnMissingBean // prevent IntelliJ warning about duplicate beans
-public abstract class ReviewDtoMapper implements DtoMapper<Review, ReviewDto> {
-    @Autowired
-    @SuppressWarnings("unused") // used in @Mapping expressions
-    protected PosService posService;
-    @Autowired
-    @SuppressWarnings("unused") // used in @Mapping expressions
-    protected UserService userService;
+public interface ReviewDtoMapper extends DtoMapper<Review, ReviewDto> {
 
-    @Mapping(target = "posId", expression = "java(source.pos().getId())")
-    @Mapping(target = "authorId", expression = "java(source.author().getId())")
-    public abstract ReviewDto fromDomain(Review source);
+    @Override
+    ReviewDto fromDomain(Review source);
 
-    @Mapping(target = "pos", expression = "java(posService.getById(source.posId()))")
-    @Mapping(target = "author", expression = "java(userService.getById(source.authorId()))")
-    @Mapping(target = "approved", constant = "false")
-    @Mapping(target = "approvalCount", constant = "0")
-    public abstract Review toDomain(ReviewDto source);
+    @Override
+    Review toDomain(ReviewDto source);
 }
